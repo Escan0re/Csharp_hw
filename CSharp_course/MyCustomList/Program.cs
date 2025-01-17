e6094848f175f5e0dd1c976256228e8517cfa367
Sergey Rogov <s.a.rogov20@gmail.com>
2025-01-17 21:02:18 +0300

    feature/my-custom-list-T

--- CSharp_course/MyCustomList/Program.cs
+++ CSharp_course/MyCustomList/Program.cs
@@ -1,6 +1,27 @@
-﻿namespace MyList;
+﻿using MyCustomList;
 
-public class Program
+var list = new MyList<int>();
+
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+list.Add([7, 8, 9]);
+list.Add(1, 999);
+list.Add(3, [1000, 2000, 3000]);
+
+for (var i = 0; i < list.Count; i++)
 {
-    
+    Console.WriteLine(list[i]);
+}
+
+Console.WriteLine("Count: " + list.Count);
+Console.WriteLine("Capacity: " + list.Capacity);
+
+Console.WriteLine("------------ сортировка ------------");
+
+list.Sort();
+for (var i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
 }
