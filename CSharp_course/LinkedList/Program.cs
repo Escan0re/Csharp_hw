82357433f9bdd0e237e65fae75fd2cad95e1e86d
Sergey Rogov <s.a.rogov20@gmail.com>
2025-01-29 18:16:07 +0300

    LinkedList

---
+++ CSharp_course/LinkedList/Program.cs
@@ -0,0 +1,11 @@
+﻿var list = new MyLinkedList();
+list.Add(10);
+list.Add(20);
+list.Add(30);
+list.Add(1, 100);
+list[2] = 99;
+
+for (var i = 0; i < list.Count; i++)
+{
+    Console.WriteLine(list[i]);
+}
