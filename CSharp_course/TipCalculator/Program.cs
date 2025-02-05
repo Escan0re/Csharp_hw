a514078ffe367023af311bd4ef35e5904882a7eb
Sergey Rogov <s.a.rogov20@gmail.com>
2025-02-05 15:24:31 +0300

    tip-calculator

---
+++ CSharp_course/TipCalculator/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите сумму счета:");
+var billAmount = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите процент чаевых:");
+var tipPercentage = decimal.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество человек в группе:");
+var numberOfPeople = int.Parse(Console.ReadLine());
+
+var totalAmount = billAmount + billAmount * tipPercentage / 100;
+var amountPerPerson = totalAmount / numberOfPeople;
+
+Console.WriteLine($"Общий счет: {totalAmount}");
+Console.WriteLine($"Процент чаевых {tipPercentage}%");
+Console.WriteLine($"С каждого: {amountPerPerson}");
