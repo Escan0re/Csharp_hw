f8ac20b807225a4f42a5a0faee7dfe5cc41e21cc
Sergey Rogov <s.a.rogov20@gmail.com>
2025-02-06 14:31:11 +0300

    Quiz

---
+++ CSharp_course/Quiz/Program.cs
@@ -0,0 +1,24 @@
+﻿var userScore = 0;
+Console.WriteLine("Что означает слово 'cat'?");
+var answer = Console.ReadLine();
+if (answer == "кошка")
+{
+    userScore += 10;
+    Console.WriteLine("Всё верно!");
+}
+else
+{
+    Console.WriteLine("Нужно подумать ещё...");
+}
+Console.WriteLine("Что означает слово 'dog'?");
+var answer1 = Console.ReadLine();
+if (answer1 == "собака")
+{
+    userScore += 10;
+    Console.WriteLine("Всё верно!");
+}
+else
+{
+    Console.WriteLine("Нужно подумать ещё...");
+}
+Console.WriteLine(userScore);
