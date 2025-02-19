9f018da498d5ae6efd694392d65e313b89884e0e
Sergey Rogov <s.a.rogov20@gmail.com>
2025-02-19 17:14:27 +0300

    Init

---
+++ CSharp_course/ObjectOriented/Program.cs
@@ -0,0 +1,13 @@
+﻿using ObjectOriented;
+
+var person1 = new Person("Ваня К.", 23);
+var person2 = new Person("Юля К.", 21);
+
+var repository = new Repository(10);
+for (var i = 0; i < 100; i++)
+{
+    repository.Append(person1);
+}
+
+
+repository.Print();
