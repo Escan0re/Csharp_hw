89c9e6df0fccd5f5173e84bcbaa0fa8e63b153fb
Sergey Rogov <s.a.rogov20@gmail.com>
2025-02-25 18:18:52 +0300

    Multithreading1

---
+++ CSharp_course/Multithreading1/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Foo {
+    private SemaphoreSlim _semSecond;
+    private SemaphoreSlim _semThird;
+    
+    public Foo() {
+        _semSecond = new SemaphoreSlim(0, 1);
+        _semThird = new SemaphoreSlim(0, 1);
+    }
+
+    public void First(Action printFirst) {
+        printFirst();
+        _semSecond.Release();
+    }
+
+    public void Second(Action printSecond) {
+        _semSecond.Wait();
+        printSecond();
+        _semThird.Release();
+    }
+
+    public void Third(Action printThird) {
+        _semThird.Wait();
+        printThird();
+    }
+}
